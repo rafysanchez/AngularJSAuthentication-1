4c03ae5e9dcebbf5641b1497d742131bce80254d
Leandro <lndr27@hotmail.com>
2017-05-21 13:35:11 -0300

    1st commit
    commit

---
+++ AngularJSAuthentication.API/AuthContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace AngularJSAuthentication.API
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            :base("AuthContext")
+        {
+
+        }
+    }
+}
---
+++ AngularJSAuthentication.API/Startup.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * reference: http://bitoftech.net/2014/06/01/token-based-authentication-asp-net-web-api-2-owin-asp-net-identity/
+ */
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(AngularJSAuthentication.API.Startup))]
+namespace AngularJSAuthentication.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
